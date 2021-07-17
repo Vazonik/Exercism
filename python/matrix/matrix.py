f8dd5337968480ad43b022b8160632448c98520f
Dawid Draguła <dawid.dragula@gmail.com>
2021-07-18 00:45:41 +0200

    Matrix v2

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,10 +1,9 @@
 class Matrix:
     def __init__(self, matrix_string):
         self.rows = [[int(j) for j in i.split(" ")] for i in matrix_string.split("\n")]
-        self.columns = [list(i) for i in zip(*self.rows)]
 
     def row(self, index):
         return self.rows[index - 1]
 
     def column(self, index):
-        return self.columns[index - 1]
+        return [list(i) for i in zip(*self.rows)][index - 1]
